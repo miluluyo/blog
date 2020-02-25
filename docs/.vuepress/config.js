3f8b47ac672d9a33da5d59e19a54bbd68efc98fb
miluluyo <miluluyo@163.com>
2020-02-25 15:44:43 +0800

    改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,7 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
-  base: '/', // 这是部署到github相关的配置 
+  base: '/blog/', // 这是部署到github相关的配置 
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
